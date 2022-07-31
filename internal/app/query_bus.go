6d2510138afd8a3d6f3340e9c4ee199c656846f8
Pablo Braulio <brulics@gmail.com>
2022-07-31 10:59:35 +0200

    Add query bus

---
+++ internal/app/query_bus.go
@@ -0,0 +1,37 @@
+package app
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/bruli/raspberryRainSensor/pkg/common/cqs"
+)
+
+type QueryBus struct {
+	m map[string]cqs.QueryHandler
+}
+
+func (c QueryBus) Handle(ctx context.Context, query cqs.Query) (cqs.QueryResult, error) {
+	hand, ok := c.m[query.Name()]
+	if !ok {
+		return nil, UnSubscribedQueryError{name: query.Name()}
+	}
+	return hand.Handle(ctx, query)
+}
+
+func NewQueryBus() QueryBus {
+	m := make(map[string]cqs.QueryHandler)
+	return QueryBus{m: m}
+}
+
+func (c QueryBus) Subscribe(name string, query cqs.QueryHandler) {
+	c.m[name] = query
+}
+
+type UnSubscribedQueryError struct {
+	name string
+}
+
+func (u UnSubscribedQueryError) Error() string {
+	return fmt.Sprintf("query %q not subscribed", u.name)
+}
---
+++ internal/app/query_bus_test.go
@@ -0,0 +1,80 @@
+package app_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/bruli/raspberryRainSensor/pkg/common/test"
+
+	"github.com/bruli/raspberryRainSensor/pkg/common/cqs"
+	"github.com/bruli/raspberryWaterSystem/internal/app"
+	"github.com/stretchr/testify/require"
+)
+
+func TestQueryBusHandle(t *testing.T) {
+	errTest := errors.New("")
+	tests := []struct {
+		name, queryName string
+		handler         cqs.QueryHandler
+		query           cqs.Query
+		expectedResult  cqs.QueryResult
+		expectedErr     error
+	}{
+		{
+			name:        "with a not subscribed query, then it returns an unsubscribed query error",
+			queryName:   "query",
+			handler:     queryHandler{},
+			query:       query{name: "unknown"},
+			expectedErr: app.UnSubscribedQueryError{},
+		},
+		{
+			name:      "with a subscribed query, then it execute handle method",
+			queryName: "query",
+			handler:   queryHandler{},
+			query:     query{name: "query"},
+		},
+		{
+			name:      "with a subscribed query, then it execute handle method and return same query error",
+			queryName: "other query",
+			handler: queryHandler{
+				err: errTest,
+			},
+			query:       query{name: "other query"},
+			expectedErr: errTest,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(`Given a QueryBus,
+		when Handle method is called `+tt.name, func(t *testing.T) {
+			t.Parallel()
+			bus := app.NewQueryBus()
+			bus.Subscribe(tt.queryName, tt.handler)
+			result, err := bus.Handle(context.Background(), tt.query)
+			if err != nil {
+				test.CheckErrorsType(t, tt.expectedErr, err)
+				require.Nil(t, result)
+				return
+			}
+			require.Equal(t, tt.expectedResult, result)
+		})
+	}
+}
+
+type query struct {
+	name string
+}
+
+func (c query) Name() string {
+	return c.name
+}
+
+type queryHandler struct {
+	result cqs.QueryResult
+	err    error
+}
+
+func (q queryHandler) Handle(ctx context.Context, query cqs.Query) (cqs.QueryResult, error) {
+	return q.result, q.err
+}
