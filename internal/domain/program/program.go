5579096c58c9bb9398a7791ad989039decbe96db
Pablo Braulio <brulics@gmail.com>
2022-08-01 16:45:45 +0200

    Add programs

--- internal/domain/program/program.go
+++ internal/domain/program/program.go
@@ -13,7 +13,7 @@ func (p Program) Execution() Execution {
 	return p.execution
 }
 
-func New(seconds Seconds, hour Hour, zones []string) (Program, error) {
+func new(seconds Seconds, hour Hour, zones []string) (Program, error) {
 	exec, err := NewExecution(hour, zones)
 	if err != nil {
 		return Program{}, err
@@ -25,4 +25,40 @@ func New(seconds Seconds, hour Hour, zones []string) (Program, error) {
 		seconds:   seconds,
 		execution: exec,
 	}, nil
+}
+
+type DailyProgram struct {
+	Program
+}
+
+func NewDaily(seconds Seconds, hour Hour, zones []string) (DailyProgram, error) {
+	pr, err := new(seconds, hour, zones)
+	if err != nil {
+		return DailyProgram{}, err
+	}
+	return DailyProgram{Program: pr}, nil
+}
+
+type OddProgram struct {
+	Program
+}
+
+func NewOdd(seconds Seconds, hour Hour, zones []string) (OddProgram, error) {
+	pr, err := new(seconds, hour, zones)
+	if err != nil {
+		return OddProgram{}, err
+	}
+	return OddProgram{Program: pr}, nil
+}
+
+type EvenProgram struct {
+	Program
+}
+
+func NewEven(seconds Seconds, hour Hour, zones []string) (EvenProgram, error) {
+	pr, err := new(seconds, hour, zones)
+	if err != nil {
+		return EvenProgram{}, err
+	}
+	return EvenProgram{Program: pr}, nil
 }
--- internal/domain/program/program_test.go
+++ internal/domain/program/program_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestNew(t *testing.T) {
+func TestNewDaily(t *testing.T) {
 	seconds, err := program.ParseSeconds(20)
 	require.NoError(t, err)
 	zones := []string{"1", "2"}
@@ -45,9 +45,111 @@ func TestNew(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(`Given a Program struct,
-		when New function is called `+tt.name, func(t *testing.T) {
+		when new function is called `+tt.name, func(t *testing.T) {
 			t.Parallel()
-			prg, err := program.New(tt.seconds, tt.hour, tt.zones)
+			prg, err := program.NewDaily(tt.seconds, tt.hour, tt.zones)
+			if err != nil {
+				test.CheckErrorsType(t, tt.expectedErr, err)
+				return
+			}
+			require.Equal(t, tt.expectedErr, err)
+			require.Equal(t, tt.hour, prg.Execution().Hour())
+			require.Equal(t, tt.seconds, prg.Seconds())
+			require.Equal(t, tt.zones, prg.Execution().Zones())
+		})
+	}
+}
+
+func TestNewODD(t *testing.T) {
+	seconds, err := program.ParseSeconds(20)
+	require.NoError(t, err)
+	zones := []string{"1", "2"}
+	hour, err := program.ParseHour("15:04")
+	require.NoError(t, err)
+	tests := []struct {
+		name        string
+		seconds     program.Seconds
+		hour        program.Hour
+		zones       []string
+		expectedErr error
+	}{
+		{
+			name:        "with an invalid seconds, then it returns an zero program seconds error",
+			seconds:     program.Seconds(time.Duration(0) * time.Second),
+			zones:       zones,
+			hour:        hour,
+			expectedErr: program.ErrZeroProgramSeconds,
+		},
+		{
+			name:        "with an empty zones, then it returns an empty execution zones error",
+			seconds:     seconds,
+			hour:        hour,
+			expectedErr: program.ErrEmptyExecutionZones,
+		},
+		{
+			name:    "with all values, then it returns a program",
+			seconds: seconds,
+			hour:    hour,
+			zones:   zones,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(`Given a Program struct,
+		when new function is called `+tt.name, func(t *testing.T) {
+			t.Parallel()
+			prg, err := program.NewOdd(tt.seconds, tt.hour, tt.zones)
+			if err != nil {
+				test.CheckErrorsType(t, tt.expectedErr, err)
+				return
+			}
+			require.Equal(t, tt.expectedErr, err)
+			require.Equal(t, tt.hour, prg.Execution().Hour())
+			require.Equal(t, tt.seconds, prg.Seconds())
+			require.Equal(t, tt.zones, prg.Execution().Zones())
+		})
+	}
+}
+
+func TestNewEven(t *testing.T) {
+	seconds, err := program.ParseSeconds(20)
+	require.NoError(t, err)
+	zones := []string{"1", "2"}
+	hour, err := program.ParseHour("15:04")
+	require.NoError(t, err)
+	tests := []struct {
+		name        string
+		seconds     program.Seconds
+		hour        program.Hour
+		zones       []string
+		expectedErr error
+	}{
+		{
+			name:        "with an invalid seconds, then it returns an zero program seconds error",
+			seconds:     program.Seconds(time.Duration(0) * time.Second),
+			zones:       zones,
+			hour:        hour,
+			expectedErr: program.ErrZeroProgramSeconds,
+		},
+		{
+			name:        "with an empty zones, then it returns an empty execution zones error",
+			seconds:     seconds,
+			hour:        hour,
+			expectedErr: program.ErrEmptyExecutionZones,
+		},
+		{
+			name:    "with all values, then it returns a program",
+			seconds: seconds,
+			hour:    hour,
+			zones:   zones,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(`Given a Program struct,
+		when new function is called `+tt.name, func(t *testing.T) {
+			t.Parallel()
+			prg, err := program.NewEven(tt.seconds, tt.hour, tt.zones)
 			if err != nil {
 				test.CheckErrorsType(t, tt.expectedErr, err)
 				return
