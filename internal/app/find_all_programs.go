a0d17bdc0749131d959b4ed9e5548617f07517d4
Pablo Braulio <brulics@gmail.com>
2022-08-03 15:48:51 +0200

    Add find all programs query

---
+++ internal/app/find_all_programs.go
@@ -0,0 +1,68 @@
+package app
+
+import (
+	"context"
+
+	"github.com/bruli/raspberryRainSensor/pkg/common/cqs"
+	"github.com/bruli/raspberryWaterSystem/internal/domain/program"
+)
+
+const FindAllProgramsQueryName = "findAllPrograms"
+
+type FindAllProgramsQuery struct{}
+
+func (f FindAllProgramsQuery) Name() string {
+	return FindAllProgramsQueryName
+}
+
+type FindAllPrograms struct {
+	daily, odd, even ProgramRepository
+	weekly           WeeklyProgramRepository
+	temperature      TemperatureProgramRepository
+}
+
+func (f FindAllPrograms) Handle(ctx context.Context, query cqs.Query) (cqs.QueryResult, error) {
+	dailies, err := f.daily.FindAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+	odd, err := f.odd.FindAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+	even, err := f.even.FindAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+	weekly, err := f.weekly.FindAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+	temp, err := f.temperature.FindAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return AllPrograms{
+		Daily:       dailies,
+		Odd:         odd,
+		Even:        even,
+		Weekly:      weekly,
+		Temperature: temp,
+	}, nil
+}
+
+func NewFindAllPrograms(
+	daily ProgramRepository,
+	odd ProgramRepository,
+	even ProgramRepository,
+	weekly WeeklyProgramRepository,
+	temperature TemperatureProgramRepository,
+) FindAllPrograms {
+	return FindAllPrograms{daily: daily, odd: odd, even: even, weekly: weekly, temperature: temperature}
+}
+
+type AllPrograms struct {
+	Daily, Odd, Even []program.Program
+	Weekly           []program.Weekly
+	Temperature      []program.Temperature
+}
---
+++ internal/app/find_all_programs_test.go
@@ -0,0 +1,141 @@
+package app_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/bruli/raspberryWaterSystem/fixtures"
+
+	"github.com/bruli/raspberryWaterSystem/internal/domain/program"
+
+	"github.com/bruli/raspberryRainSensor/pkg/common/test"
+	"github.com/stretchr/testify/require"
+
+	"github.com/bruli/raspberryWaterSystem/internal/app"
+
+	"github.com/bruli/raspberryRainSensor/pkg/common/cqs"
+)
+
+func TestFindAllProgramsHandle(t *testing.T) {
+	errTest := errors.New("")
+	dailies := []program.Program{
+		fixtures.ProgramBuilder{}.Build(),
+	}
+	odds := []program.Program{
+		fixtures.ProgramBuilder{}.Build(),
+	}
+	evens := []program.Program{
+		fixtures.ProgramBuilder{}.Build(),
+	}
+	weeklies := []program.Weekly{
+		fixtures.WeeklyBuilder{}.Build(),
+	}
+	temps := []program.Temperature{
+		fixtures.TemperatureBuilder{}.Build(),
+	}
+	tests := []struct {
+		name  string
+		query cqs.Query
+		expectedErr, dailyErr,
+		oddErr, evenErr,
+		weeklyErr, tempErr error
+		expectedResult     cqs.QueryResult
+		dailies, odd, even []program.Program
+		weeklies           []program.Weekly
+		temps              []program.Temperature
+	}{
+		{
+			name:        "and daily repository returns an error, then it returns same error",
+			dailyErr:    errTest,
+			expectedErr: errTest,
+		},
+		{
+			name:        "and odd repository returns an error, then it returns same error",
+			oddErr:      errTest,
+			expectedErr: errTest,
+			dailies: []program.Program{
+				fixtures.ProgramBuilder{}.Build(),
+			},
+		},
+		{
+			name:        "and even repository returns an error, then it returns same error",
+			evenErr:     errTest,
+			expectedErr: errTest,
+			dailies:     dailies,
+			odd:         odds,
+		},
+		{
+			name:        "and weekly repository returns an error, then it returns same error",
+			weeklyErr:   errTest,
+			expectedErr: errTest,
+			dailies:     dailies,
+			odd:         odds,
+			even:        evens,
+		},
+		{
+			name:        "and temperature repository returns an error, then it returns same error",
+			tempErr:     errTest,
+			expectedErr: errTest,
+			dailies:     dailies,
+			odd:         odds,
+			even:        evens,
+			weeklies:    weeklies,
+		},
+		{
+			name:     "then it returns a valid result",
+			dailies:  dailies,
+			odd:      odds,
+			even:     evens,
+			weeklies: weeklies,
+			temps:    temps,
+			expectedResult: app.AllPrograms{
+				Daily:       dailies,
+				Odd:         odds,
+				Even:        evens,
+				Weekly:      weeklies,
+				Temperature: temps,
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(`Given a FindAllPrograms query handler,
+		when Handle method is called `+tt.name, func(t *testing.T) {
+			t.Parallel()
+			temperature := &TemperatureProgramRepositoryMock{
+				FindAllFunc: func(ctx context.Context) ([]program.Temperature, error) {
+					return tt.temps, tt.tempErr
+				},
+			}
+			daily := &ProgramRepositoryMock{
+				FindAllFunc: func(ctx context.Context) ([]program.Program, error) {
+					return tt.dailies, tt.dailyErr
+				},
+			}
+			odd := &ProgramRepositoryMock{
+				FindAllFunc: func(ctx context.Context) ([]program.Program, error) {
+					return tt.odd, tt.oddErr
+				},
+			}
+			even := &ProgramRepositoryMock{
+				FindAllFunc: func(ctx context.Context) ([]program.Program, error) {
+					return tt.even, tt.evenErr
+				},
+			}
+			weekly := &WeeklyProgramRepositoryMock{
+				FindAllFunc: func(ctx context.Context) ([]program.Weekly, error) {
+					return tt.weeklies, tt.weeklyErr
+				},
+			}
+			handler := app.NewFindAllPrograms(daily, odd, even, weekly, temperature)
+			result, err := handler.Handle(context.Background(), tt.query)
+			if err != nil {
+				test.CheckErrorsType(t, tt.expectedErr, err)
+				return
+			}
+			require.Equal(t, tt.expectedErr, err)
+			require.Equal(t, tt.expectedResult, result)
+		})
+	}
+}
